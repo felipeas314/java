7df6d87e96ee9302ce9b3ce68641c9e8dfd768cd
Felipe Alexandre <felipeas314@gmail.com>
2022-06-26 09:40:51 -0300

    feat: add promotion dto

---
+++ spring-web-flux-aggregation/src/main/java/br/com/labs/dto/Promotion.java
@@ -0,0 +1,37 @@
+package br.com.labs.dto;
+
+import java.time.LocalDate;
+
+public class Promotion {
+
+	private String type;
+
+	private Double discount;
+
+	private LocalDate endDate;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(Double discount) {
+		this.discount = discount;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+}
