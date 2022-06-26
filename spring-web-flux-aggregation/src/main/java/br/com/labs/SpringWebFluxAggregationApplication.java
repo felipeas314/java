e11104dbf308c6fe0bd37ec381c2f69b04c2a346
Felipe Alexandre <felipeas314@gmail.com>
2022-06-26 09:25:01 -0300

    feat: first commit

---
+++ spring-web-flux-aggregation/src/main/java/br/com/labs/SpringWebFluxAggregationApplication.java
@@ -0,0 +1,13 @@
+package br.com.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxAggregationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxAggregationApplication.class, args);
+	}
+
+}
