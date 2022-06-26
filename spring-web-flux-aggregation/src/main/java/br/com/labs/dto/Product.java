05366035865aa7a7758ec1b19a8f0cdb2779898a
Felipe Alexandre <felipeas314@gmail.com>
2022-06-26 09:39:42 -0300

    feat: add product dto

---
+++ spring-web-flux-aggregation/src/main/java/br/com/labs/dto/Product.java
@@ -0,0 +1,35 @@
+package br.com.labs.dto;
+
+public class Product {
+
+	private String id;
+
+	private String description;
+
+	private String category;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+}
