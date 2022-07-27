4c8cdfb9b982cdc7fdcd3e38d6114599760a5f51
Felipe Alexandre <felipeas314@gmail.com>
2022-07-27 20:41:23 -0300

    teste

--- api-spring-r2dbc/src/main/java/br/com/labs/ApiSpringR2dbcApplication.java
+++ api-spring-r2dbc/src/main/java/br/com/labs/ApiSpringR2dbcApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiSpringR2dbcApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiSpringR2dbcApplication.class, args);
+
+		//fazendo o teste do commit
 	}
 
 }
